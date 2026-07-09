5350e8282e38687f2bedf8af770396823d23e49b
darklsor56 <dmr42203@gmail.com>
2026-07-09 03:22:23 -0700

    Fix middleware to use json() instead of json

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // Middleware
 app.use(express.urlencoded({ extended: true}));
-app.use(express.json);
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Temporary routes
