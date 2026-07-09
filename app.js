03130cff3a6653213c4d0ff692ef584fd8e5a7f4
darklsor56 <dmr42203@gmail.com>
2026-07-09 02:49:09 -0700

    Add temporary routes for home, listings, and user authentication

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json);
 app.use(express.static(path.join(__dirname, "public")));
+
+// Temporary routes
+app.get("/", (req, res) => {res.render("home", { title: "DiceShare" })});
+app.get("/listings", (req, res) => {res.render("listings", { title: "Browse Listings" })});
+app.get("/listings/new", (req, res) => {res.render("create-listing", { title: "Create Listing" })});
+app.get("/listings/:id", (req, res) => {res.render("listing-detail", { title: "Listing Detail" })});
+app.get("/profile", (req, res) => {res.render("profile", { title: "Profile" })});
+app.get("/login", (req, res) => {res.render("login", { title: "Log In" })});
+app.get("/register", (req, res) => {res.render("register", { title: "Register" })});
+app.listen(PORT, () => {console.log(`DiceShare running on https://localhost:${PORT}`)})
