e03f16773ef94a4a2b8c15c3dd83619cf09b9bba
darklsor56 <dmr42203@gmail.com>
2026-07-09 02:39:35 -0700

    Initialize Express app and set up view engine

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 import express from "express";
 import path from "path";
 require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// View Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
