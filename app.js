b500824dd5d2ea2fe0ffb2bc3ab5a46331307887
darklsor56 <dmr42203@gmail.com>
2026-07-11 20:54:12 -0700

    connected database to app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
+const connectDB = require("./config/db");
 require("dotenv").config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
