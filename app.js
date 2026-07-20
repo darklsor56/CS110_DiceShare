15f2a1d01d21fd499aeee4d579982160e63dfa1d
darklsor56 <dmr42203@gmail.com>
2026-07-20 02:11:39 -0700

    make the  route async

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get("/listings/new", (req, res) => {res.render("create-listing", { title: "C
 app.get("/listings/:id", (req, res) => {res.render("listing-detail", { title: "Listing Detail" })});
 app.get("/profile", (req, res) => {res.render("profile", { title: "Profile" })});
 app.get("/login", (req, res) => {res.render("login", { title: "Log In" })});
-app.get("/register", (req, res) => {
+app.get("/register", async(req, res) => {
   try {
     const { username, email, location, bio, password, confirmPassword } = req.body;
 
